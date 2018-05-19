3067215eaeea2b169a44144acba4c9722acdcd35
pierre <perso@pierreboissinot.me>
2018-05-19 10:20:13 +0200

    add home template

---
+++ src/Controller/ManagerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ManagerController extends AbstractController
+{
+    /**
+     * @Route("/", defaults={}, methods={"GET"}, name="manager_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('manager/index.html.twig');
+    }
+    
+}
